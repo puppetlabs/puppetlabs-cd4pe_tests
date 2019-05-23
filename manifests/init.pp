8021b0ae74741274a698f31faa37241731618cf6
ericwilliamson <eric.williamson@puppet.com>
2019-05-23 11:09:21 -0700

    (maint) Add regex module tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,6 @@
 #
 class cd4pe_tests {
   notify { "test_notify":
-    message => "notify from puppetlabs-cd4pe_tests#module_deployment_src",
+    message => "notify from puppetlabs-cd4pe_tests#module_regex_tests",
   }
 }

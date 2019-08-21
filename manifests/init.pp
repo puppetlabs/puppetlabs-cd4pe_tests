8b1371cadea7c7994e725c50327c0d2f620a6ed1
Chris Leicester <chrisleicester@gmail.com>
2019-08-21 14:02:39 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,6 @@
 #
 class cd4pe_tests {
   notify { "test_notify":
-    message => "notify from puppetlabs-cd4pe_tests#rdm_integration_tests_git_based_modules_update_5",
+    message => "notify from puppetlabs-cd4pe_tests#rdm_integration_tests_git_based_modules_update_6",
   }
 }

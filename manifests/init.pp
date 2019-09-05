39675d007d327129b5e84a2843f04bef678cbfd6
Eric Williamson <1686271+ericwilliamson@users.noreply.github.com>
2019-09-05 14:27:55 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,6 @@
 #
 class cd4pe_tests {
   notify { "test_notify":
-    message => "notify from puppetlabs-cd4pe_tests#master",
+    message => "notify from puppetlabs-cd4pe_tests#git_based_module_test",
   }
 }
